6468eb58322d58f4af4bc0c6c5a53b14714cf3af
Marcia Paige <mapaige7@gmail.com>
2020-09-12 20:30:03 -0400

    Added add, view, update functions

--- 02-Homework/seed.sql
+++ 02-Homework/seed.sql
@@ -5,67 +5,25 @@ CREATE DATABASE employee_db;
 USE employee_db;
  
 CREATE TABLE department (
- id INT NOT NULL  AUTO INCREMENT,
-  name VARCHAR (30) NULL,
-
-);
-
-CREATE TABLE role (
-  id INT NOT NULL AUTO INCREMENT,
-  title VARCHAR (30),
-  salary DECIMAL,
-  department_id INT, 
-  PRIMARY KEY (id),
-);
-
-CREATE TABLE employee (
-  id INT NOT NULL AUTO INCREMENT,
-  first_name VARCHAR (30),
-  last_name VARCHAR (30),
-  role_id INT ,
-  manager_id INT,
+ id INT  AUTO_INCREMENT NOT NULL ,
+  section VARCHAR (30) NULL,
   PRIMARY KEY (id)
 );
 
+CREATE TABLE role (
+  id INT  AUTO_INCREMENT NOT NULL,
+  title VARCHAR (30),
+  salary DECIMAL NULL,
+   FOREIGN KEY department_id INT NULL, 
+  PRIMARY KEY (id)
+);
 
-INSERT INTO department (name)
-VALUES ("HR"), ("Tech"), ("Sales"),("Operations");
+CREATE TABLE employee (
+  id INT  AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR (30),
+  last_name VARCHAR (30),
+  role_id  INT ,
+manager_id INT ,
+  PRIMARY KEY (id)
+);
 
-INSERT INTO role(title, salary, department_id)
-VALUES("Benefits","75000", 1), ("Web Dev", "80000", 2), ("Sales Manager","100000",3),("Payroll","75000", 4) ;
-
-INSERT INTO employee (first_name, last_name, role_id, manager)
-VALUES ("Jeff", "Miller","Sales Lead",),("Mariah", "Cooper","Salesperson", 2), ("Constance", "Battle","Secretary", 3), ("Jacob", "Langston", 3);
-
-
-SELECT 
-
-e. id AS id,
-e. first_name AS First,
-e. last_name AS Last,
-e. role_id as Role,
-s. salary as Salary,
-m. last_name AS Manager,
-d. name AS Department 
-
-
--- JOIN employee as employee
-
-FROM employee e
-LEFT JOIN employee m 
-ON e.manager_id = manager.id 
-
--- JOIN role table to employee table 
-LEFT JOIN role r 
-ON e.role_id =  s.title
-
---  JOIN department to role table
-
-LEFT JOIN department d 
-ON r.department_id = d.id 
-
-
-
-
-
- 
--- 02-Homework/server.js
+++ 02-Homework/server.js
@@ -1,4 +1,181 @@
-var inquirer = require("inquirer");
-var mysql = require("mysql");
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+var consTable = require('console.table');
+
+// Add connection to SQL
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+
+	// Your port; if not 3306
+	port: 3306,
+	// your username
+
+	user: 'root',
+
+	// Your password
+	password: 'linequeen1',
+	database: 'employee_db',
+});
+
+// Connect to mySql server and databased
+
+connection.connect(function (err) {
+	if (err) throw err;
+	console.log('connectd as id  ' + connection.threadId);
+	runSearch();
+});
+
+// Functionality of application
+
+function runSearch() {
+	inquirer
+		.prompt({
+			name: 'action',
+			type: 'list',
+			message: 'What would you like to do?',
+			choices: ['View', 'Add', 'Update', 'Exit'],
+		})
+		.then(function (res) {
+			switch (res.action) {
+				case 'View':
+					view();
+					break;
+
+				case 'Add':
+					add();
+					break;
+
+				case 'Update':
+					updateEmployee();
+					break;
+
+				case 'Exit':
+					console.table('____________________________');
+					console.log('All done');
+					console.table('________________________');
+					break;
+				default:
+					console.log('default');
+			}
+		});
+}
+function add() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'add',
+      message:'Please select to add',
+      choices: ['All employees', 'By department', 'By role'],
+    }
+  ]).then(answer =>{
+    switch (answer.add){
+      case 'All employees':
+					addAllEmployees();
+					break;
+				case 'By department':
+					addByDepartment();
+					break;
+				case 'By role':
+					addByRole();
+				default:
+					console.log('default');
+      }
+    });
+}
+
+function add() {
+  inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'update',
+      message:'Please select to update',
+      choices: ['All employees', 'By department', 'By role'],
+    }
+  ]).then(answer =>{
+    switch (answer.update){
+      case 'All employees':
+					updateAllEmployees();
+					break;
+				case 'By department':
+					updateByDepartment();
+					break;
+				case 'By role':
+					updateByRole();
+				default:
+					console.log('default');
+      }
+    });
+}
+function addByDepartment(){
+  inquirer
+  .prompt([
+    {
+      type:'input',
+      message:'What is your department?',
+      name: 'section',
+    }
+  ])
+  .then(function(res){
+    connection.query("INSERT into department(section)VALUES(?)",res.section,function(err){
+      console.log("Department added")
+      runSearch();
+    })
+  })
+}
+// View function
+
+function view() {
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'view',
+				message: 'Please select one to view',
+				choices: ['All employees', 'By department', 'By role'],
+			},
+		])
+		.then(function (res) {
+			switch (res.view) {
+				case 'All employees':
+					viewAllEmployees();
+					break;
+				case 'By department':
+					viewByDepartment();
+					break;
+				case 'By role':
+					viewByRole();
+				default:
+					console.log('default');
+			}
+		});
+}
+
+function viewAllEmployees() {
+	connection.query('SELECT * FROM employee', function (err, res) {
+		if (err) throw err;
+		console.table(res);
+	runSearch();
+	});
+}
+function viewByDepartment() {
+	connection.query('SELECT * FROM department', function (err, res) {
+		if (err) throw err;
+		console.table(res);
+		runSearch();
+	});
 
 
+}
+
+
+
+function viewByRole() {
+  connection.query('SELECT * FROM role', function (err, res) {
+    if (err) throw err;
+    console.table(res);
+    runSearch();
+  });
+}
